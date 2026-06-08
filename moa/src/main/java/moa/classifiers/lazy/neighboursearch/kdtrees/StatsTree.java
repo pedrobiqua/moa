87803ee2c600def137d3fcf0aa0172b1ee68af5c
Pedro Bianchini de Quadros <pedrobiqua@outlook.com>
2026-06-08 19:05:11 -0300

    adicionando nova metrica

--- moa/src/main/java/moa/classifiers/lazy/neighboursearch/kdtrees/StatsTree.java
+++ moa/src/main/java/moa/classifiers/lazy/neighboursearch/kdtrees/StatsTree.java
@@ -1,11 +1,15 @@
 package moa.classifiers.lazy.neighboursearch.kdtrees;
 
+import java.util.TreeSet;
+
 public class StatsTree {
     /** Dados da árvore **/
     public int m_numNodes, m_heightTree, m_numNodesDeleted;
 
     /** Metricas reiniciaveis coletadas **/
     public int depthInsert, depthSearch, backtrack;
+
+    public TreeSet<Integer> visitedNodes = new TreeSet<Integer>();
 
     /** Metricas de contagens **/
     public int countRebuild = 0;
@@ -14,7 +18,7 @@ public class StatsTree {
     public double timeInsert, timeRebuild, timeSearch;
 
     public String getHeader() {
-        return "m_numNodes,m_heightTree,m_numNodesDeleted,depthInsert,depthSearch,backtrack,countRebuild";
+        return "m_numNodes,m_heightTree,m_numNodesDeleted,depthInsert,depthSearch,visitedNodes,backtrack,countRebuild";
     }
 
     public String getMetrics() {
@@ -23,6 +27,7 @@ public class StatsTree {
                 + m_numNodesDeleted + ","
                 + depthInsert + ","
                 + depthSearch + ","
+                + visitedNodes.size() + ","
                 + backtrack + ","
                 + countRebuild;
     }
